36f8fdc7df32f4a2116c002c516e6d385b8c3331
Dezső Horsi <dezso.horsi@gmail.com>
2023-04-08 19:10:31 +0200

    dataFEtoBE, imgFEtoTemp, imgFEtoBE, imgPUBLIC+web

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,8 +1,46 @@
-
-
 console.log('loaded')
 
-fetch('http://127.0.0.1:9000/data')
+const formComponent = () => `
+  <form>
+  <input type="text" name="name" placeholder="image name">
+  <input type="file" name="file">
+  <button>send</button> 
+  </form>
+`
+
+const rootElement = document.querySelector('#root')
+rootElement.insertAdjacentHTML('beforeend', formComponent())
+
+const formElement = document.querySelector('form')
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log('submit')
+
+  const formData = new FormData()
+  formData.append('name', document.querySelector("input[type='text']").value)
+  formData.append('image', document.querySelector("input[type='file']").files[0])
+
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(res => {
+    if (res.status === 200) {
+      console.log('success')
+      return res.json() // return  "pelda" itt szedjük ki az értéket, itt adjuk tovább a köv.then-nek
+    } else {
+      console.log(('ERROR!!!'))
+    }
+  })
+  .then(resData => { // ebben a callback fn-ben resData néven fogadjuk az előző callback fn visszatérését: resData = "pelda". Így közlekedik az adatom a backend és frontend között.
+    rootElement.insertAdjacentHTML('beforeend', 
+    `<img src="./public/${resData}.jpg">`)
+  })
+  .catch(error => console.log(error))
+})
+
+/* fetch('http://127.0.0.1:9000/data') 
 .then(response => { 
   console.log(response)
   if (response.status === 200) {
@@ -16,4 +54,4 @@ fetch('http://127.0.0.1:9000/data')
     console.log(element.name)
     document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`)
   });
-})
+}) */
