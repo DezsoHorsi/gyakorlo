b7201c5a3ce5f4821208c1d1677447ec2b251472
Dezső Horsi <dezso.horsi@gmail.com>
2023-04-13 17:23:11 +0200

    additional comments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express') //1. ez keresi meg az express-t és importálja be, függvénnyel visszatér egy értékkel
 const path = require('path') // ez is egy importálás, ez teszi lehetővé a fájlrendszerben a mozgást.
-const fs = require('fs')
+const fs = require('fs') //5.6 ez keresi meg az fs-t
 const app = express() //2. az elérési út kimentése változóba
 const port = 9000 //ez a kód végén van behívva az app.listenbe
 
@@ -11,32 +11,32 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(`${__dirname}/../frontend/public`)) //2. end point: statikus nyilvános mappa elérhetővé tétele
 
 app.get('/data', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/data/data.json`)) //3. end point: backend data.json mappáját érjük el vele
+  res.sendFile(path.join(`${__dirname}/data/data.json`)) //4. end point: backend data.json mappáját érjük el vele
 })
-
-app.get('/data/:id', (req, res) => {
+// 5. ADATOK ELÉRÉSE ID ALAPJÁN
+app.get('/data/:id', (req, res) => { //5.1. ha pl. a böngibe beírjuk: http://127.0.0.1:9000/data/1, kiadja az első objektumot, mert elértünk vele a backendünkhöz.
 	//console.log(req.params)
-	try {
-		const searchId = parseInt(req.params.id)
+	try { //5.2 String átalakítása (bármit küldünk, az a backenden a data.json objektumában stringként jelenik meg, a szám is.)
+		const searchId = parseInt(req.params.id) //ezzel alakítunk stringgé
 		//console.log(searchId)
 
-		if(isNaN(searchId)) {
-			res.status(400).send("NaN")
-		} else {
-			fs.readFile(`data/data.json`, (err, data) => {
+		if(isNaN(searchId)) { //5.3 Ez kell, mert ha a böngibe a data/ után stringet írunk NaN-t ad, ezért kell ez a 400-as hibakód (bad request)
+			res.status(400).send("NaN") 
+		} else { //5.4 else-ággal fedjük le azt, ha numbert kérünk be.
+			fs.readFile(`data/data.json`, (err, data) => { //5.5 json fájlokat olvasunk, fs (filesystem)-fájlokat importálunk
 				let result = null
-				const fileData = JSON.parse(data)
+				const fileData = JSON.parse(data) //itt a fileData igazi tömb lett, amin végigiterálhatunk
 				//console.log(fileData)
 				for (let index = 0; index < fileData.length; index++) {
 					const element = fileData[index];
 					if(element.id === searchId) {
 						console.log(element)
-						result = element
+						result = element //itt adja vissza a lekért id-jú elementet
 					}
 				}
 
 				if(result === null) {
-					res.status(404).send("nincs ilyen érték")
+					res.status(404).send("nincs ilyen érték") //ha a megadott id nem létezik, akkor ezt adja vissza
 				} else {
 					res.send(result)
 				}
@@ -49,6 +49,6 @@ app.get('/data/:id', (req, res) => {
 	}
 })
 
-app.listen(port, () => { //egy figyelő, itt mondjuk meg az appnak, hogy figyelje a portot, majd kiírja terminalba a szerver linkjét, így klikkelhetünk rá és megnyílik a böngiben, és megjelenik a hello world a viewporton (html-ből)
+app.listen(port, () => { //3. egy figyelő, itt mondjuk meg az appnak, hogy figyelje a portot, majd kiírja terminalba a szerver linkjét, így klikkelhetünk rá és megnyílik a böngiben, és megjelenik a hello world a viewporton (html-ből)
   console.log(`http://127.0.0.1:${port}`)
 })
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -2,18 +2,18 @@
 
 console.log('loaded')
 //ADAT KIÍRATÁSA A BÖNGÉSZŐBE BACKENDBŐL (data.json)
-fetch('http://127.0.0.1:9000/data') //feltárcsázzuk a szervert a data-val
-.then(response => { // itt kapunk egy csomó adatot a konzolba backendről
+fetch('http://127.0.0.1:9000/data') //1. feltárcsázzuk a szervert a data-val, hogy hozzájussunk az adatunkhoz
+.then(response => { // 2. itt kapunk egy response-t, egy csomó adatot a konzolba backendről
   console.log(response) 
   if (response.status === 200) {
-    console.log('OK') // a böngésző konzoljában kiírja, hogy OK
+    console.log('OK') // 3. hibakód megírása (if), a böngésző konzoljában kiírja, hogy OK
   }
-  return response.json()
+  return response.json() //4. a response-ból (az adatokból) csinálunk egy json-t
 })
-.then(responseJson => {  // itt fogjuk elérni az adatokat, csak akkor fut le, ha létezik adat a data.json fájlban
-  const data = responseJson
-  data.forEach(element => {
+.then(responseJson => {  //5. itt már hivatkozunk a response.json()-ra, itt fogjuk elérni az adatokat, csak akkor fut le, ha létezik adat a data.json fájlban
+  const data = responseJson //6. az adatokat kimentjük egy data nevű változóba
+  data.forEach(element => { //7. végigiterálunk az adatokon és kiíratjuk konzolra a sör nevét (element.name)
     console.log(element.name)
-    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`)
+    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`) //8. DOM-manipulálás azért, hogy a sör nevét a viewportba is kiírja.
   });
 })
